8facbec7894bdb283b113d61c9d548f4ce19ed70
asanbekovmyrzajan1@gmail.com <asanbekovmyrzajan1@gmail.com>
2024-06-22 02:21:30 +0600

    the wrong code is there

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from "react";
-import "./index.css";
-import WordleBoard from "./components/WordleBoard/WordleBoard";
-import "./App.css"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import WordG from "./pages/WordG/WordG";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Wordle</h1>
-      <WordleBoard/>
-    </div>
-  );
+export default function App() {
+    const [tile, setTile] = useState<string>("");
+    const [loading, setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchRandomWord = async () => {
+            setLoading(true);
+
+            try {
+                const response = await fetch(
+                    "https://piccolo-server.vercel.app/words"
+                );
+                const data = await response.json();
+                const words: string[] = data.data;
+                const randomIndex: number = Math.floor(
+                    Math.random() * words.length
+                );
+                const randomWord: string = words[randomIndex];
+                setTile(randomWord);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching random word:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchRandomWord();
+    }, []);
+
+    return (
+        <div className="App">
+          <WordG/>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/pages/WordG/WordG.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useWordle } from "../../hooks/useWordle";
+import { Tile } from "../../components/Tile/Tile";
+import "./WordG.css";
+
+const WordG = () => {
+  const {
+    word,
+    isLoading,
+    messsage,
+    guesses,
+    gameOver,
+    currentGuesses,
+    currentIndex,
+    getTileClass,
+    initializeGame,
+  } = useWordle();
+  console.log(word);
+
+  return (
+    <div className="word-container">
+        <h1>Wordle-Game</h1>
+      {isLoading ? (
+        <p>Загрузка...</p>
+      ) : (
+        <div>
+          {Array.from({ length: 6 }, (_, row) => (
+            <div key={row} className="row">
+              {Array.from({ length: 5 }, (_, col) => {
+                const index = row * 5 + col;
+                const inputValue = guesses[row][col] || "";
+                const tileClass =
+                  currentIndex > row || gameOver ? getTileClass(inputValue, col, row) : "";
+                return (
+                  <Tile
+                    key={index}
+                    index={index}
+                    inputValue={inputValue}
+                    tileClass={tileClass}
+                  />
+                );
+              })}
+            </div>
+          ))}
+          {messsage &&  <p style={{color: 'white'}}>Вы проиграли правильное слово: {word}</p> }
+          {gameOver && (
+            <div>
+              <button onClick={initializeGame}>Переключиться на новую игру!</button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WordG;
